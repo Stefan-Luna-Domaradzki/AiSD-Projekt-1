198c2d12cd606d55a49ae97442077c62dccb69ac
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2021-11-04 17:07:02 +0100

    Int main
    Początkowa funkcja

--- AiSD-Projekt-1/AiSD-Projekt-1/main.cpp
+++ AiSD-Projekt-1/AiSD-Projekt-1/main.cpp
@@ -2,3 +2,18 @@
 // 166642 Stefan Domaradzki 
 // Temat: Dla zadanej tablicy liczb ca³kowitych o rozmiarze n zawieraj¹cej wartoœæi z przedzia³u [1,n]
 //		  znajdŸ element powtarzaj¹cy siê.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+	cout << "Start";
+	system("Pause");
+	return 0;
+
+
+
+}
