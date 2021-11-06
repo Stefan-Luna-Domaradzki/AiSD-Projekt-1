e5eda4b343f2b9fa2296c96f3178425fe7bb898d
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2021-11-06 18:35:09 +0100

    zaimplementowane 2 algorytmy wyszukujące powtarzających się liczb
    nie uwzględniają jeszcze sytuacji skrajnych jak brak liczb, wyłącznie jedna liczba itp.

--- AiSD-Projekt-1/AiSD-Projekt-1/main.cpp
+++ AiSD-Projekt-1/AiSD-Projekt-1/main.cpp
@@ -11,12 +11,12 @@
 
 using namespace std;
 
-void wypelnij(int tablica[], int rozmiar);
-void wypisz(int tablica[], int rozmiar, double czas, string algorytm);
+void wypelnij(int tab[], int rozmiar);
+void wypisz(int tab[], int rozmiar, double czas, string algorytm);
 
 
-void brute_force(int tablica[], int rozmiar);
-void bucket_sort(int tablica[], int rozmiar);
+void brute_force(int tab[], int rozmiar);
+void bucket_sort(int tab[], int rozmiar);
 
 int main(){
 	
@@ -49,20 +49,20 @@ int main(){
 
 }
 
-void wypelnij(int tablica[], int rozmiar)
+void wypelnij(int tab[], int rozmiar)
 {
 	srand(time(NULL));
 	for (int i = 0; i < rozmiar; i++)
 	{
-		tablica[i] = rand()% rozmiar + 1;
+		tab[i] = rand()% rozmiar + 1;
 	}
 }
 
-void wypisz(int tablica[], int rozmiar, double czas, string algorytm)
+void wypisz(int tab[], int rozmiar, double czas, string algorytm)
 {
 	for (int i = 0; i < rozmiar; i++)
 	{
-		cout << tablica[i] << setw(5);
+		cout << tab[i] << setw(5);
 	}
 
 	ofstream dane;
@@ -72,10 +72,51 @@ void wypisz(int tablica[], int rozmiar, double czas, string algorytm)
 	dane.close();
 }
 
-void brute_force(int tablica[], int rozmiar)
+void brute_force(int tab[], int rozmiar)
 {
+	int szukaj;
+	int porownaj;
+
+	for (int i = 0; i < rozmiar; i++)
+	{
+		szukaj = tab[i];
+		porownaj = 0;
+
+		for (int j = 0; j < rozmiar; j++)
+		{
+			if (tab[j] == szukaj) porownaj++;
+
+			if (porownaj == 2)
+			{
+				cout << "liczba która sie powtarza to: " << szukaj << ". \n";
+				break;
+			}
+			
+		}
+
+	}
 }
 
-void bucket_sort(int tablica[], int rozmiar)
+void bucket_sort(int tab[], int rozmiar)
 {
+	//z za³o¿enia ¿e liczby s¹ z przedzia³u 1 do n (rozmiar) 
+	//tablica wymagana do sortowania kube³kowego powinna byæ rozmiaru n
+	//pozycja zerowa nowej tablicy zawsze pozostanie pusta i bêdzie ignorowana
+
+	int* buckets = new int[rozmiar + 1];
+	buckets = { 0 };
+
+	for (int i = 0; i < rozmiar; i++)
+	{
+		buckets[tab[i]]++;
+
+		if (buckets[tab[i]] = 2) 
+		{
+			cout << "liczba która sie powtarza to: " << tab[i] << ". \n";
+			break;
+		}
+
+	}
+
+
 }
